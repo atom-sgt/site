c3841b1c9dab24a3f7a5e1dde4da4bc314f2fa8b
Adam Sargent <adamcsargent@gmail.com>
2022-01-30 20:32:41 -0500

    Updates

---
+++ precache-manifest.33ccdca6a7a2130ba0e39e55d8a96262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318c6a35db6e5dacedc250899e5b68ea",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "d38b7830e6e946a285ee",
+    "url": "/site/static/css/main.0a9052b7.chunk.css"
+  },
+  {
+    "revision": "e7e8c66db4eab6cfbb6f",
+    "url": "/site/static/js/2.112374e0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/site/static/js/2.112374e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38b7830e6e946a285ee",
+    "url": "/site/static/js/main.738d6ee2.chunk.js"
+  },
+  {
+    "revision": "a6f832edb43cb75d95e7",
+    "url": "/site/static/js/runtime-main.6815615a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/site/precache-manifest.5c9633bd930659c0330b178cc33f0c1f.js"
+  "/site/precache-manifest.33ccdca6a7a2130ba0e39e55d8a96262.js"
 );
 
 self.addEventListener('message', (event) => {
